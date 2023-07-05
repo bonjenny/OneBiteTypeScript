f9633623814c9d1ab58a0093435b3b704885f156
Jihee Eom <bonjenny1@gmail.com>
2023-07-05 20:35:57 +0900

    Exercises 8번 완료

---
+++ 타입스크립트 연습/8.ts
@@ -0,0 +1,88 @@
+/**
+ * 7/5(수)
+ * 타입스크립트 Exercises 8번
+ * https://typescript-exercises.github.io/#exercise=8&file=%2Findex.ts
+ * =============================================================
+ */
+
+interface User {
+  type: "user";
+  name: string;
+  age: number;
+  occupation: string;
+}
+
+interface Admin {
+  type: "admin";
+  name: string;
+  age: number;
+  role: string;
+}
+
+type PowerUser = {
+  type: "powerUser";
+  name: string;
+  age: number;
+  occupation: string;
+  role: string;
+};
+
+export type Person = User | Admin | PowerUser;
+
+export const persons: Person[] = [
+  {
+    type: "user",
+    name: "Max Mustermann",
+    age: 25,
+    occupation: "Chimney sweep",
+  },
+  { type: "admin", name: "Jane Doe", age: 32, role: "Administrator" },
+  { type: "user", name: "Kate Müller", age: 23, occupation: "Astronaut" },
+  { type: "admin", name: "Bruce Willis", age: 64, role: "World saver" },
+  {
+    type: "powerUser",
+    name: "Nikki Stone",
+    age: 45,
+    role: "Moderator",
+    occupation: "Cat groomer",
+  },
+];
+
+function isAdmin(person: any): person is Admin {
+  return person.type === "admin";
+}
+
+function isUser(person: any): person is User {
+  return person.type === "user";
+}
+
+function isPowerUser(person: any): person is PowerUser {
+  return person.type === "powerUser";
+}
+
+export function logPerson(person: Person) {
+  let additionalInformation: string = "";
+  if (isAdmin(person)) {
+    additionalInformation = person.role;
+  }
+  if (isUser(person)) {
+    additionalInformation = person.occupation;
+  }
+  if (isPowerUser(person)) {
+    additionalInformation = `${person.role}, ${person.occupation}`;
+  }
+  console.log(`${person.name}, ${person.age}, ${additionalInformation}`);
+}
+
+console.log("Admins:");
+persons.filter(isAdmin).forEach(logPerson);
+
+console.log();
+
+console.log("Users:");
+persons.filter(isUser).forEach(logPerson);
+
+console.log();
+
+console.log("Power users:");
+persons.filter(isPowerUser).forEach(logPerson);
