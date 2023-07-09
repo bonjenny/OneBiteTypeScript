ebf903b6c65f46d9abb6413fb133df5237b45a87
Jihee Eom <bonjenny1@gmail.com>
2023-07-09 19:15:06 +0900

    1-3) 타입스크립트의 동작 원리 완료

--- 타입스크립트 연습/10.ts
+++ 타입스크립트 연습/10.ts
@@ -46,37 +46,8 @@ export type ApiResponse<T> =
       error: string;
     };
 
-type CallbackBasedAsyncFunction<T> = (
-  callback: (response: ApiResponse<T>) => void
-) => void;
-type PromiseBasedAsyncFunction<T> = () => Promise<T>;
-
-export function promisify<T>(
-  fn: CallbackBasedAsyncFunction<T>
-): PromiseBasedAsyncFunction<T> {
-  return () =>
-    new Promise<T>((resolve, reject) => {
-      fn((response) => {
-        if (response.status === "success") {
-          resolve(response.data);
-        } else {
-          reject(new Error(response.error));
-        }
-      });
-    });
-}
-
-type SourceObject<T> = { [K in keyof T]: CallbackBasedAsyncFunction<T[K]> };
-type PromisifiedObject<T> = { [K in keyof T]: PromiseBasedAsyncFunction<T[K]> };
-
-export function promisifyAll<T extends { [key: string]: any }>(
-  obj: SourceObject<T>
-): PromisifiedObject<T> {
-  const result: Partial<PromisifiedObject<T>> = {};
-  for (const key of Object.keys(obj) as (keyof T)[]) {
-    result[key] = promisify(obj[key]);
-  }
-  return result as PromisifiedObject<T>;
+export function promisify(arg: unknown): unknown {
+  return null;
 }
 
 const oldApi = {
@@ -108,7 +79,14 @@ const oldApi = {
   },
 };
 
-export const api = promisifyAll(oldApi);
+export const api = {
+  requestAdmins: promisify(oldApi.requestAdmins),
+  requestUsers: promisify(oldApi.requestUsers),
+  requestCurrentServerTime: promisify(oldApi.requestCurrentServerTime),
+  requestCoffeeMachineQueueLength: promisify(
+    oldApi.requestCoffeeMachineQueueLength
+  ),
+};
 
 function logPerson(person: Person) {
   console.log(
