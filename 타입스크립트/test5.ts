2c14c6d69e0629e85208fe17987522f0d8733b45
bonjenny <bonjenny1@gmail.com>
2023-11-06 09:43:30 +0900

    11/6 - 특정 문자 개수 구하는 2가지 방법

--- 타입스크립트/test5.ts
+++ 타입스크립트/test5.ts
@@ -7,3 +7,8 @@ const prop = {} as any;
 //   const file_cnt = _.vSplit(prop['z_file_attach$z_file_attach$file_des'], '∬')?.length;
 //   prop['inv_s_list$slip_link_cnt'] = prop['inv_s_list$slip_link_cnt'] + file_cnt;
 // }
+
+/**
+ * [JavaScript] 문자열에서 특정 문자 개수 구하는 2가지 방법
+ * https://gent.tistory.com/467
+ */
