16ebdc05810f94805a7f11649c91df746345a7ad
Jihee Eom <bonjenny1@gmail.com>
2024-01-02 20:00:47 +0900

    2-8) Void와 Never 타입

---
+++ 한 입 크기로 잘라먹는 타입스크립트/SECTION2/src/2-8) Void와 Never 타입.ts
@@ -0,0 +1,14 @@
+/**
+ * 1/2(화)
+ * * 2-8) Void와 Never 타입
+ * https://www.inflearn.com/course/lecture?courseSlug=%ED%95%9C%EC%9E%85-%ED%81%AC%EA%B8%B0-%ED%83%80%EC%9E%85%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8&unitId=155997
+ * =============================================================
+ */
+
+// * <<Void 타입>>
+// void란 공허, 즉 아무것도 없음
+// 아무것도 없다는 걸 의미하는 타입
+
+function func1(): string {
+  return "hello";
+}
