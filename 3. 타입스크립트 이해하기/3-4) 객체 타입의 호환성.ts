37f0526e29b613d261f8ffda0dd442bff73a92a5
Jihee Eom <bonjenny1@gmail.com>
2024-02-25 15:47:29 +0900

    2.25 3-4) 객체 타입간의 호환성

---
+++ 3. 타입스크립트 이해하기/3-4) 객체 타입의 호환성.ts
@@ -0,0 +1,10 @@
+/**
+ * 2/25(일)
+ * * 3-4) 객체 타입의 호환성
+ * https://www.inflearn.com/course/lecture?courseSlug=%ED%95%9C%EC%9E%85-%ED%81%AC%EA%B8%B0-%ED%83%80%EC%9E%85%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8&unitId=156632&tab=curriculum
+ * =============================================================
+ */
+
+/**
+ * * <<기본 타입간의 호환성>>
+ */
